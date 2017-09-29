e382de9f14f2bf7a8e1db20dbb6358c5b9984769
sivakumarkatam <siva.katam@blazeclan.com>
2017-09-29 15:57:43 +0530

    Create V3_1__put_data.sql

---
+++ migrationSqlScripts/V3_1__put_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Persons (Id,Name,Phone,Address) VALUES ("1","name is gone","040","Hyd");
+INSERT INTO Persons (Id,Name,Phone,Address) VALUES ("2","name is gone","020","pune");
+INSERT INTO Persons (Id,Name,Phone,Address) VALUES ("3","name is gone","012","bang");
+INSERT INTO Persons (Id,Name,Phone,Address) VALUES ("4","name is gone","0192","mumbai");
