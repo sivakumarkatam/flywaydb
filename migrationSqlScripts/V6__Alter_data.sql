34872ad555906ed8e5bf03f193b70aa928c6535e
sivakumarkatam <siva.katam@blazeclan.com>
2017-10-06 10:34:43 +0530

    Update V6__Alter_data.sql

--- migrationSqlScripts/V6__Alter_data.sql
+++ migrationSqlScripts/V6__Alter_data.sql
@@ -1,4 +1,3 @@
 DELETE FROM demo WHERE demo_color='red';
-DELETE FROM Test WHERE id=1;
 INSERT INTO FOO (FOO_ID, FOO_VALUE) VALUES (2, 'This is added ');
 INSERT INTO FOO (FOO_ID, FOO_VALUE) VALUES (3, 'This is added by siva kumar');
